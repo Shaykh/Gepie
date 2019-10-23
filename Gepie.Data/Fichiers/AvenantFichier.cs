99a621c1b8c44dee0387ab08642d221127107b6b
Diaby Cheick <diabycheick@live.fr>
2019-10-23 11:56:39 +0200

    Ajout de nouvelles entités

---
+++ Gepie.Data/Fichiers/AvenantFichier.cs
@@ -0,0 +1,7 @@
+namespace Gepie.Data
+{
+    public class AvenantFichier : Fichier
+    {
+        public Avenant Avenant { get; set; }
+    }
+}
---
+++ Gepie.Data/Fichiers/ContratBailFichier.cs
@@ -0,0 +1,7 @@
+namespace Gepie.Data
+{
+    public class ContratBailFichier : Fichier
+    {
+        public ContratBail Contrat { get; set; }
+    }   
+}
